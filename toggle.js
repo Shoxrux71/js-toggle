38bd94b4a27a5229e369176c617d98777dfe7421
Shoxrux71 <amirjonovdamin@gmail.com>
2024-02-17 18:22:53 +0500

    new

--- toggle.js
+++ toggle.js
@@ -1,2 +1,13 @@
-let box = document.querySelectorAll(".text");
-console.log(box);
+function changeText() {
+    let id = document.getElementsByName("button")[0].id;
+    if (id == 1) {
+        document.getElementById("text").innerHTML = "日本語を 知っていますか?";
+        document.getElementById("text").style.backgroundColor = "red";
+        document.getElementsByName("button")[0].id = 0;
+    }
+    else {
+        document.getElementById("text").innerHTML = "oxwamadi bleeet!";
+        document.getElementById("text").style.backgroundColor = "green";
+        document.getElementsByName("button")[0].id = 1;
+    }
+}
